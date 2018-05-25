bffc8d9cd0b33fdb89aa0cc4411653049b988b2b
Antério vieira <anteriovieira@gmail.com>
2018-05-24 23:00:29 -0300

    feat(tests) updated

---
+++ test/fixture/nuxt.config.2.js
@@ -0,0 +1,15 @@
+const { resolve } = require('path')
+
+module.exports = {
+    rootDir: resolve(__dirname, '../..'),
+    srcDir: __dirname,
+    dev: false,
+    render: {
+        resourceHints: false
+    },
+    modules: [
+        ['@@', {
+            resources: '~/assets/sass/colors.scss'
+        }]
+    ],
+}
--- test/module.test.js
+++ test/module.test.js
@@ -1,13 +1,54 @@
 const { Nuxt, Builder } = require('nuxt')
 const request = require('request-promise-native')
 
-const config = require('./fixture/nuxt.config')
-
 const url = (path) => `http://localhost:3000${path}`
 const get = (path) => request(url(path))
 
-describe('basic', () => {
+describe('Config mode 1', () => {
     let nuxt
+    const config = require('./fixture/nuxt.config.1')
+
+    beforeAll(async () => {
+        nuxt = new Nuxt(config)
+        await new Builder(nuxt).build()
+        await nuxt.listen(3000)
+    }, 60000)
+
+    afterAll(async () => {
+        await nuxt.close()
+    })
+
+    test('render', async () => {
+        let html = await get('/')
+        expect(html).toContain('color:#35495e;')
+        expect(html).toContain('background:#42b883')
+    })
+})
+
+describe('Config mode 2', () => {
+    let nuxt
+    const config = require('./fixture/nuxt.config.2')
+
+    beforeAll(async () => {
+        nuxt = new Nuxt(config)
+        await new Builder(nuxt).build()
+        await nuxt.listen(3000)
+    }, 60000)
+
+    afterAll(async () => {
+        await nuxt.close()
+    })
+
+    test('render', async () => {
+        let html = await get('/')
+        expect(html).toContain('color:#35495e;')
+        expect(html).toContain('background:#42b883')
+    })
+})
+
+describe('Config mode 3', () => {
+    let nuxt
+    const config = require('./fixture/nuxt.config.3')
 
     beforeAll(async () => {
         nuxt = new Nuxt(config)
